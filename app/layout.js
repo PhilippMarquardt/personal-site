019a566353ad02848e473e61b10976571af9e258
PhilippMarquardt <p.marquardt15@googlemail.com>
2024-07-12 12:42:47 +0200

    .

--- app/layout.js
+++ app/layout.js
@@ -3,19 +3,25 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import 'katex/dist/katex.min.css'
 import 'highlight.js/styles/github.css';
-
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Philipp Marquardt",
   description: "Personal Site",
+  verification: {
+    google: "53T7bzBWPmqBSicdukWSBp07YmonNWlZbGM1_gZhimQ",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}<SpeedInsights /></body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
