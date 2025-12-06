7ac64b4d7d768738f0db6d063528005e54499791
PhilippMarquardt <p.marquardt15@googlemail.com>
2025-12-06 15:53:46 +0100

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
-import next from 'next';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            "sharp$": false,
-            "onnxruntime-node$": false,
-        }
-        return config;
-    },
-}
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "sharp$": false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
+
+  // Explicitly disable Turbopack
+  experimental: {
+    turbo: false
+  }
+};
 
 export default nextConfig;
